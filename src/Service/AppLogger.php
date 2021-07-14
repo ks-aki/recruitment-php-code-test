7433a104c35d37d696e56b07fb59bdbb074702aa
ks-aki <aki.zhou@ks-it.co>
2021-07-14 16:05:01 +0800

    '提交'

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Service;
+use think\facade\Log;
 
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
+    const TYPE_THINK_LOG = 'think_log';
 
     private $logger;
 
@@ -12,6 +14,20 @@ class AppLogger
     {
         if ($type == self::TYPE_LOG4PHP) {
             $this->logger = \Logger::getLogger("Log");
+        }
+        if ($type == self::TYPE_THINK_LOG){
+            Log::init(
+                [
+                    'default'	=>	'file',
+                    'channels'	=>	[
+                        'file'	=>	[
+                            'type'	=>	'file',
+                            'path'	=>	'./logs/',
+                        ],
+                    ],
+                ]
+            );
+            $this->logger = Log::instance();
         }
     }
 
--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -2,7 +2,46 @@
 
 namespace App\Service;
 
-class ProductHandler
-{
+class ProductHandler{
+
+    /**
+     * 获取商品总额
+     * @param $products
+     * @return float|int
+     */
+    public function getTotalPrice($products){
+        $price = array_column($products,'price');
+        return array_sum($price);
+
+    }
+
+    /**
+     * 按分类筛选商品，并按价格从大大小排序
+     * @param $products
+     * @param $type
+     * @return bool
+     */
+    public function filterProducts($products,$type){
+        $product = [];
+        foreach ($products as $item){
+            if ($item['type'] == $type){
+                $product[] = $item;
+            }
+        }
+        function sort($a,$b){
+            if ($a['price'] == $b['price']) return 0;
+            return $a['price'] > $b['price'] ? 1 : -1;
+        }
+        return usort($product,'sort');
+    }
+
+    /**
+     * 日期格式转换成时间戳
+     * @param $date
+     * @return false|int
+     */
+    public function dateToTimestamp($date){
+        return strtotime($date);
+    }
 
 }
