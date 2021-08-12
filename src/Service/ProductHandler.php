4b350d81e67e25abb680b2c70cb3eb25aaeab017
chenhaidi <184826832@qq.com>
2021-08-12 15:56:57 +0800

    面试题

--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -2,7 +2,43 @@
 
 namespace App\Service;
 
+use const http\Client\Curl\Features\UNIX_SOCKETS;
+
 class ProductHandler
 {
+    public function getTotalPrice($products = array())
+    {
+        if(empty($propucts)) {
+            return false;
+        }
+        $totalPrice = 0;
+        foreach ($propucts as $v){
+            $price = $product['price'] ?: 0;
+            $totalPrice += $price;
+        }
+        return $totalPrice;
+    }
+
+
+    public function arrSort($products = array())
+    {
+        if(empty($propucts)) {
+            return false;
+        }
+        return arsort($products);
+    }
+
+
+    public function updateTime($products = array())
+    {
+        if(empty($propucts)) {
+            return false;
+        }
+        foreach ($products as $k => $v) {
+            $products[$k]['create_at'] = strtotime($v['create_at']);
+        }
+        return $products;
+    }
+
 
 }
---
+++ tests/Service/DemoTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Test\Service;
+
+use App\Apppp\Demo as DemoClass;
+
+/**
+ * Class DemoTest
+ */
+class DemoTest extends DemoClass
+{
+
+    public function testGetUserInfo()
+    {
+
+    }
+}
+
